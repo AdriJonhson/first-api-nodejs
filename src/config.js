9c2ef28d43ac4177bd717736253cba512b349392
Adri Silva <30940624+AdriJonhson@users.noreply.github.com>
2018-07-20 20:48:06 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, Seja bem-vindo ao Node Store!';
 
 module.exports = {
-	connectionString: 'mongodb://adri:parede13@ds018168.mlab.com:18168/nodestore',
-	sendgridKey: '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',
+	connectionString: 'MONGLAB',
+	sendgridKey: 'SENDGRID_KEY',
 	containerConnectionString: 'TBD'
 }
